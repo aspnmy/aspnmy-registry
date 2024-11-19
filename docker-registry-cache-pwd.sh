da33da2952293e1706ae35f9e7b732d1d9e99029
aspnmy <support@e2bank.cn>
2024-11-20 04:45:41 +0800

    更新docker-compose代码

--- docker-registry-cache-pwd.sh
+++ docker-registry-cache-pwd.sh
@@ -91,7 +91,7 @@ update_docker_env(){
     log "更新aspnmy-registry-cache初始参数完成"
 }
 
-set_docker_compane_file(){
+set_docker_compose_file(){
 
 # 文件名
 FILE_NAME="$BASE_DIR/config/docker-registry.yml"
@@ -108,8 +108,8 @@ services:
             # 配置缓存模式
             - $BASE_DIR/config/proxy-config-en.yml:/etc/docker/registry/config.yml:ro
             # 配置ssl证书此处为目录模式
-            #- /etc/letsencrypt/live/$DOMAIN/fullchain.pem:/certs/fullchain.pem
-            #- /etc/letsencrypt/live/$DOMAIN/privkey.pem:/certs/privkey.pem
+            - /etc/letsencrypt/live/$DOMAIN/fullchain.pem:/certs/fullchain.pem
+            - /etc/letsencrypt/live/$DOMAIN/privkey.pem:/certs/privkey.pem
             # 配置仓库实际挂载地址
             - /opt/aspnmy_registry/registry_data:/var/lib/registry
         environment:
@@ -160,7 +160,7 @@ main() {
     # 更新基础配置
     update_docker_env
     # 设置docker-compose配置脚本
-    set_docker_compane_file
+    set_docker_compose_file
     # 拉起主服务镜像
     runAspnmyRegistryCache
     log "所有设置完成"
